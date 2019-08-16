e7a9d4382ab8f4adcf965088b2f2c59933845966
ka <karine73@gmail.com>
2019-08-16 23:50:28 +0200

    ajout texte page accueil

--- Views/home/HomeComponent.js
+++ Views/home/HomeComponent.js
@@ -10,21 +10,23 @@ import styles from './home.style';
 export default class HomeComponent extends React.Component {
   render() {
     return (
-        <View style={styles.container}>
-          <View style={styles.containerImage}>
-            <Image source={require('../../assets/img/accueil_img.jpg')}
-                   style={styles.mainImage}/>
-          </View>
-          <View style={styles.containerTitre}>
-            <Text style={[styles.mainTitle]}>Restaurant Soleil  </Text>
-            <Text style={[styles.mainTitle]}>Votre restaurant Vietnamien</Text>
-            <Text style={[styles.mainTitle]}>Menu</Text>
-          </View>
-          {/*<ImageBackground source={...}*/}
-          {/*                 style={{ width: '100%', height: '100%' }}>*/}
-          {/*  <Text>Inside</Text>*/}
-          {/*</ImageBackground>*/}
+      <View style={styles.container}>
+        <View style={styles.containerImage}>
+          <Image source={require('../../assets/img/accueil_img.jpg')}
+            style={styles.mainImage} />
         </View>
+        <View style={styles.containerTitre}>
+          <Text style={[styles.mainTitle]}>Restaurant Soleil  </Text>
+          <Text style={[styles.mainTitle]}>2 rue d'along 57070 Metz, France</Text>
+          <Text style={[styles.mainTitle]}>06 28 65 56 65</Text>
+          <Text style={[styles.mainTitle]}>HORAIRES</Text>
+          <Text style={[styles.mainTitle]}>Ouvert 7/7 de 11h30 à 23h30</Text>
+        </View>
+        {/*<ImageBackground source={...}*/}
+        {/*                 style={{ width: '100%', height: '100%' }}>*/}
+        {/*  <Text>Inside</Text>*/}
+        {/*</ImageBackground>*/}
+      </View>
     );
   }
 }
