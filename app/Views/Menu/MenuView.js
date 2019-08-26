0dd50212b6a2618f09f4d4bde0ad9b5a6304461a
Thierry Aronoff <thierry.aronoff@gmail.com>
2019-08-26 22:46:33 +0200

    Alert on click on item

--- app/Views/Menu/MenuView.js
+++ app/Views/Menu/MenuView.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {
   ActivityIndicator,
+  Alert,
   ScrollView,
   View,
 } from 'react-native';
@@ -72,6 +73,59 @@ class MenuView extends React.Component {
 
   _onClick(el) {
     console.log(el);
+
+    let choice;
+
+    switch (el) {
+      case '0': {
+
+        choice = 'Entrées';
+        break;
+      }
+
+      case 1: {
+        choice = 'Plats principal';
+        break;
+      }
+      case 2: {
+        choice = 'Desserts';
+        break;
+      }
+      case 3: {
+        choice = 'Menus';
+        break;
+      }
+      case 4: {
+        choice = 'Spécialités';
+        break;
+      }
+      case 6: {
+        choice = 'Plats du jour';
+        break;
+      }
+      default:
+        choice = 'Aucun choix';
+
+    }
+
+    Alert.alert(
+        'Alert Title',
+        `Votre choix : ${choice}`,
+        [
+          {
+            text: `Vous avez choisi : ${choice}`,
+            onPress: () => console.log('Ask me later pressed'),
+          },
+          {
+            text: 'Cancel',
+            onPress: () => console.log('Cancel Pressed'),
+            style: 'cancel',
+          },
+          { text: 'OK', onPress: () => console.log('OK Pressed') },
+        ],
+        { cancelable: false },
+    );
+
   }
 }
 
